c66a4916d86949f29839a7f71fc2da89eda95bc4
v.niznevskiy@unitpay.ru <gQaWsEd123#h>
2020-03-12 14:52:28 +0300

    Added domains support

--- payment/unitpay/order.php
+++ payment/unitpay/order.php
@@ -4,52 +4,53 @@
 if(empty($GLOBALS['SysValue'])) exit(header("Location: /"));
 
 
-// регистрационная информация
+// СЂРµРіРёСЃС‚СЂР°С†РёРѕРЅРЅР°СЏ РёРЅС„РѕСЂРјР°С†РёСЏ
+$domain = $SysValue['unitpay']['domain'];
 $public_key = $SysValue['unitpay']['public_key'];
 $secret_key = $SysValue['unitpay']['secret_key'];
 
 
-// параметры магазина
+// РїР°СЂР°РјРµС‚СЂС‹ РјР°РіР°Р·РёРЅР°
 /*$mrh_ouid = explode("-", $_POST['ouid']);
-$inv_id = $mrh_ouid[0]."".$mrh_ouid[1];     //номер счета*/
+$inv_id = $mrh_ouid[0]."".$mrh_ouid[1];     //РЅРѕРјРµСЂ СЃС‡РµС‚Р°*/
 $id = $_POST['ouid'];
 
-// описание покупки
+// РѕРїРёСЃР°РЅРёРµ РїРѕРєСѓРїРєРё
 $inv_desc  = "PHPShopPaymentService";
 
-// сумма покупки
+// СЃСѓРјРјР° РїРѕРєСѓРїРєРё
 $out_summ  = number_format($GLOBALS['SysValue']['other']['total'], 2, '.', '');
 
-// код валюты в заказе
+// РєРѕРґ РІР°Р»СЋС‚С‹ РІ Р·Р°РєР°Р·Рµ
 $mnt_currency = $GLOBALS['PHPShopSystem']->getDefaultValutaIso();
 
-// библиотека корзины
+// Р±РёР±Р»РёРѕС‚РµРєР° РєРѕСЂР·РёРЅС‹
 $PHPShopCart = new PHPShopCart();
 
 /**
- * Шаблон вывода таблицы корзины
+ * РЁР°Р±Р»РѕРЅ РІС‹РІРѕРґР° С‚Р°Р±Р»РёС†С‹ РєРѕСЂР·РёРЅС‹
  */
 function cartpaymentdetails($val) {
-     $dis=$val['uid']."  ".$val['name']." (".$val['num']." шт. * ".$val['price'].") -- ".$val['total']."
+     $dis=$val['uid']."  ".$val['name']." (".$val['num']." С€С‚. * ".$val['price'].") -- ".$val['total']."
 ";
 
     return $dis;
 }
 
-// вывод HTML страницы с кнопкой для оплаты
+// РІС‹РІРѕРґ HTML СЃС‚СЂР°РЅРёС†С‹ СЃ РєРЅРѕРїРєРѕР№ РґР»СЏ РѕРїР»Р°С‚С‹
 $disp= '
 <div align="center">
 <p>
-Платежи через сервис <b>Unitpay</b> – это хороший выбор.
+РџР»Р°С‚РµР¶Рё С‡РµСЂРµР· СЃРµСЂРІРёСЃ <b>Unitpay</b> вЂ“ СЌС‚Рѕ С…РѕСЂРѕС€РёР№ РІС‹Р±РѕСЂ.
 </p>
  <p><br></p>
  
-<form method="GET" name="pay" id="pay" action="https://unitpay.ru/pay/' . $public_key . '">
+<form method="GET" name="pay" id="pay" action="https://' . $domain . '/pay/' . $public_key . '">
     <input type="hidden" name="account" value="'.$id.'">
     <input type="hidden" name="sum" value="'.$out_summ.'">
     <input type="hidden" name="currency" value="'.$mnt_currency.'">
     <input type=hidden name="desc" value="'.iconv('windows-1251','utf-8',$PHPShopCart->display('cartpaymentdetails')).'">
-        <table><tr><td><img src="images/shop/icon-client-new.gif"  width="16" height="16" border="0" align="left"><a href="javascript:pay.submit();">Оплатить через платежную систему</a></td></tr></table>
+        <table><tr><td><img src="images/shop/icon-client-new.gif"  width="16" height="16" border="0" align="left"><a href="javascript:pay.submit();">РћРїР»Р°С‚РёС‚СЊ С‡РµСЂРµР· РїР»Р°С‚РµР¶РЅСѓСЋ СЃРёСЃС‚РµРјСѓ</a></td></tr></table>
 </form>
 </div>';
 
